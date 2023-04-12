7a0d6603648a9657059f7d4153440de63ff01ab0
yoo <edel1212@naver.com>
2023-04-12 21:37:37 +0900

    그대로 출력하기

---
+++ src/stringQuize/Q11718.java
@@ -0,0 +1,21 @@
+package stringQuize;
+
+import java.io.*;
+
+public class Q11718 {
+    /**
+     * 입력 받은 대로 출력하는 프로그램을 작성하시오.
+     * */
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+            do{
+                bw.write(br.readLine());
+                bw.newLine();
+            } while(br.ready());
+            bw.flush();
+        }catch (IOException io){
+            io.printStackTrace();
+        }
+    }
+}
