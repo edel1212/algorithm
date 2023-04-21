640a7d189c5ac17ca2e4146b5bca965f6d9f20b3
yoo <edel1212@naver.com>
2023-04-21 11:48:01 +0900

    행렬 더셈

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,8 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-      Double aa = 3.2844827586206895;
 
-        System.out.println(
-                (double)((int) (aa * 1_000_000))/1_000_000
-        );
     }
 }
+
 
