373f5f5f382304fdb390da27b644cc47c453cab6
yoo <edel1212@naver.com>
2023-04-27 22:33:36 +0900

    세탁소 사장 동력

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String aa  = "1090";
-        System.out.println(aa.charAt(0));
+        int aa  = 124;
+        System.out.println(124 / (0.25*100));
     }
 }
 
