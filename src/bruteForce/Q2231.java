7dfd96e5847bce28b6ad1afe01a8d754c307844c
yoo <edel1212@naver.com>
2024-11-05 22:25:01 +0900

    구현 성공

--- src/bruteForce/Q2231.java
+++ src/bruteForce/Q2231.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Q2231 {
     /**
@@ -13,22 +14,30 @@ public class Q2231 {
      * 물론, 어떤 자연수의 경우에는 생성자가 없을 수도 있다. 반대로, 생성자가 여러 개인 자연수도 있을 수 있다.
      *
      * 자연수 N이 주어졌을 때, N의 가장 작은 생성자를 구해내는 프로그램을 작성하시오.
+     *
+     * Input : 216   // Out Put : 198
      * */
     public static void main(String[] args) {
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
-            // 분해합은 N과 N을 이루는 각 자리수의 합을 의미한다. :: 245 - > 256(=245+2+4+5)
 
-            int N = Integer.valueOf(br.read());
-
-            // 198 = 198 + 9 + 8 = 216
-            // 216 = 216 + 2 + 1 + 6
-
-            for(int i = 0 ; i < N; i ++ ){
-
-
-
-            }//for
+            int M = Integer.parseInt(br.readLine());
+            for(int i = 1 ; i <= M ; i++){
+                String numStr = String.valueOf(i);
+                int length = numStr.length();
+                int[] digits = new int[length];
+                // 받아온 숫자각각의 배열에 저장
+                for (int o = 0; o < length; o++) {
+                    digits[o] =Character.getNumericValue(numStr.charAt(o));
+                }// for
+               int arrSum =  Arrays.stream(digits).reduce(Integer::sum).getAsInt();
+                int result = i + arrSum;
+                if(M == result) {
+                    bw.write(String.valueOf(i));
+                    bw.flush();
+                    break;
+                }// if
+            } // for
 
        } catch (Exception ex){
            ex.printStackTrace();
