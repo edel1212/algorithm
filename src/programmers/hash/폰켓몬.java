9021c455780f91d7d9f903944b3d0f01e18d633a
yoo <edel1212@naver.com>
2025-04-25 22:15:02 +0900

    폰켓몬 update

--- src/programmers/hash/폰켓몬.java
+++ src/programmers/hash/폰켓몬.java
@@ -1,5 +1,7 @@
 package programmers.hash;
 
+import java.util.HashMap;
+
 public class 폰켓몬 {
     public static void main(String[] args) {
         int[] nums = {3,1,2,3};
@@ -8,16 +10,18 @@ public class 폰켓몬 {
     }
 
     public int solution(int[] nums) {
-        int answer = 0;
+        int answer;
 
-        // 문제 : 가장 다양한 수로 포켓몬을 고를 수 있는 방법
-        // 전제 조건 :  받은 배열의 N/2
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        // 1. 배열을 2로 나눠서 가지 수를 고른다.
-        // 2. 대상 배열의 중복을 제거한다.
-        // 3 . 첫번째 아이템을 key 로 잡고 value 에는 가장 다양했던 종류의 size를 넣는다.
-        // 4 . key를 돌면서 value의 size가 가장 높은것을 찾는다.
-        // 5 . 반환한다.
+        for(int i : nums){
+            // 값이 있을 경우 +1
+            map.put(i, map.getOrDefault(i,0) + 1);
+        } // loop
+
+        int size = nums.length / 2;
+
+        answer = map.size() >= size ?  size : map.size();
 
         return answer;
     }
