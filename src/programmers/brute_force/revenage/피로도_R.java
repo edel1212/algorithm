1e4619e3efca7c6759b066a8ee70d8bb37fc777e
yoo <edel1212@naver.com>
2025-06-12 21:45:35 +0900

    피로도 재풀이

---
+++ src/programmers/brute_force/revenage/피로도_R.java
@@ -0,0 +1,22 @@
+package programmers.brute_force.revenage;
+
+public class 피로도_R {
+    boolean[] visited;
+    int result = 0;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0, k , dungeons);
+        return result;
+    }
+
+    public void dfs(int depth, int stress, int[][] dungeons){
+        for(int i = 0 ; i < dungeons.length; i++){
+            if( !visited[i]  && dungeons[i][0] <= stress ){
+                visited[i] = true;
+                dfs(depth + 1, stress - dungeons[i][1], dungeons );
+                visited[i] = false;
+            } // if
+        }// for
+        result = Math.max(depth, result);
+    }
+}
