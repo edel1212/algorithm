d5752a0256d478ce8c0ce77a6858e731f8b6c90b
yoo <edel1212@naver.com>
2025-09-05 22:51:05 +0900

    스택으로 수열 만들기 진행 중

---
+++ src/do_it/quiz/스택으로_수열_만들기.java
@@ -0,0 +1,16 @@
+package do_it.quiz;
+
+import java.io.*;
+
+public class 스택으로_수열_만들기{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 입력값 N을 받음
+
+        // for 1 <= N
+        // for문 내부
+        // 값을 받음
+    }
+}
