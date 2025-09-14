5e765b220ba436e8c725f64948d3df76204105ec
yoo <edel1212@naver.com>
2025-09-14 16:58:15 +0900

    버블 소트

--- src/do_it/quiz/버블_소트.java
+++ src/do_it/quiz/버블_소트.java
@@ -1,17 +1,61 @@
 package do_it.quiz;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class 버블_소트 {
     public static void main(String[] args)  throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 아이디어가 중요한 문제다
+        // 수열의 개수
+        int N = Integer.parseInt(br.readLine());
+        // 원본 배열 생성
+        Foo[] arr = new Foo[N];
+        // 배열 초기화
+        for(int i = 0 ; i < N ; i++){
+            int value = Integer.parseInt(br.readLine());
+            arr[i] = new Foo(i, value);
+        }// for
 
-        // 수열의 개수가 최대 5,000,000 이기에 일반 버블 정렬로는 풀기 무리
-        // 아이디어 : 버블 정렬의 최대 이동 횟수는 1개이다 (반복문이 1칸씩 이동)
-        // 일반 정렬을 통해 값을 구하고 해당 값을 통해 정렬전 배열과 index를 비교해서 최대 거리 값 을 찾으면 그게 swap 횟수가 나옴
-        // 결과로 +1을 해주는 방식이기에 +1을 해줘서 출력하자
+        // 정렬 O(nlongn)
+        Arrays.sort(arr);
+
+        // 최고 이동 횟수
+        int maxMoveCount = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < N ;i++){
+            // 현재 위치에서 얼마나 당겨졌는지, 멀어졌는데 값의 차이를 -i 를 통해 계산함
+            maxMoveCount = Math.max(maxMoveCount, arr[i].index - i );
+        } // for
+
+        bw.write(String.valueOf(maxMoveCount + 1));
+        bw.flush();
+        bw.close();
+    }
+
+
+    /**
+     * 인덱스 번호와 값을 갖는 class
+     */
+    public static class Foo implements Comparable<Foo> {
+        int index;
+        int value;
+
+        public Foo(int index, int value){
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Foo o) {
+            // 오름차순 정렬
+            return this.value - o.value;
+        }
+
+        @Override
+        public String toString(){
+            return "index : " + this.index + " ___ value : " + this.value;
+        }
     }
 }
