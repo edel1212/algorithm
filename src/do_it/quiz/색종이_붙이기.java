daf58da97a63d3bec1967b89ef046f911af8aa96
yoo <edel1212@naver.com>
2025-10-08 22:13:40 +0900

    색중이 붙이기 풀이 중

---
+++ src/do_it/quiz/색종이_붙이기.java
@@ -0,0 +1,19 @@
+package do_it.quiz;
+
+import java.io.*;
+
+public class 색종이_붙이기 {
+    // 초기 색종이 판
+    static int[][] M = new int[10][10];
+    // 남은 색종이 수 ( 1x1 ~ 5x5 ) - 0은 인덱스 부분이므로 제외
+    static int[] S = {0,5,5,5,5,5};
+    static int result = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+    }
+}
