f5c2bc35d4be459f4a53418266cf704f4c2199e3
yoo <edel1212@naver.com>
2026-01-02 22:32:21 +0900

    칵테일 내용 보완

--- src/do_it/quiz/칵테일.java
+++ src/do_it/quiz/칵테일.java
@@ -7,10 +7,10 @@ import java.util.StringTokenizer;
 
 public class 칵테일 {
 
-    // 그래프
-    static ArrayList<Node>[] adj;
-    // 각 재료의 양
-    static long[] ans;
+    // 각 재료의 연결 관계(비율)을 저장할 그래프
+    static ArrayList<Node>[] recipeGraph;
+    // 최종적으로 계산될 각 재료의 양
+    static long[] ingredientAmounts;
     // 방문 여부
     static boolean visited[];
 
@@ -22,45 +22,51 @@ public class 칵테일 {
         int N = Integer.parseInt(br.readLine());
 
         // 그래프 초기화
-        adj = new ArrayList[N];
-        for (int i = 0; i < N; i++) adj[i] = new ArrayList<>();
+        recipeGraph = new ArrayList[N];
+        for (int i = 0; i < N; i++) recipeGraph[i] = new ArrayList<>();
         // 방문 여부 초기화
         visited = new boolean[N];
         // 각 재료들의 양 초기화
-        ans = new long[N];
+        ingredientAmounts = new long[N];
 
-        // 모든 비율들의 최소 공배수 초기값
-        long lcmValue = 1;
+        // 모든 비율들의 최소공배수 (나눗셈 시 소수점이 나오지 않게 하려는 '여유분'으로 1을 지정)
+        long baseMultiplier = 1;
 
         // 각각의 재료 비율울 저장
         for(int i = 0 ; i < N - 1 ; i++){
             StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(stringTokenizer.nextToken());
-            int b = Integer.parseInt(stringTokenizer.nextToken());
-            int p = Integer.parseInt(stringTokenizer.nextToken());
-            int q = Integer.parseInt(stringTokenizer.nextToken());
+            // 재료 A
+            int ingredientA = Integer.parseInt(stringTokenizer.nextToken());
+            // 재료 B
+            int ingredientB = Integer.parseInt(stringTokenizer.nextToken());
+            // 비율 A
+            int ratioA = Integer.parseInt(stringTokenizer.nextToken());
+            // 비율 B
+            int ratioB = Integer.parseInt(stringTokenizer.nextToken());
 
-            // 각각의 관계 저장
-            adj[a].add(new Node(b,p,q));
-            adj[b].add(new Node(a,q,p));
+            // 각각의 관계 저장 (A:B = ratioA:ratioB 관계 저장)
+            recipeGraph[ingredientA].add(new Node(ingredientB, ratioA ,ratioB));
+            recipeGraph[ingredientB].add(new Node(ingredientA, ratioB ,ratioA));
 
-            // 분모가 될 수 있는 값드르이 최소 공배수를 미리 저장
-            lcmValue *= (p * q / gcd(p, q));
+            // 모든 비례식의 숫자들의 최소공배수를 미리 구해둠 (가장 안전하게 모든 분모를 커버하기 위해서)
+            baseMultiplier *= ratioA * ratioB / gcd(ratioA, ratioB);
         } // for
 
-        // 이건 왜 해주는거야?
-        ans[0] = lcmValue;
+        // 첫 번째 재료의 양을 아주 넉넉한 값(최소공배수)으로 초기 설정
+        ingredientAmounts[0] = baseMultiplier;
         dfs(0);
 
-        long totalGcd = ans[0];
+        // 최소 정수를 구하는 작업
+        long commonDivisor = ingredientAmounts[0];
         for (int i = 1; i < N; i++) {
-            totalGcd = gcd(totalGcd, ans[i]);
+            // totalIngredients에는 dfs를 통해 값이 들어가 있음
+            commonDivisor = gcd(commonDivisor, ingredientAmounts[i]);
         }
 
         // 전체를 최대공약수로 나누어 가장 작은 정수비로 만듦
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            sb.append(ans[i] / totalGcd).append(" ");
+            sb.append(ingredientAmounts[i] / commonDivisor).append(" ");
         }
         System.out.println(sb.toString().trim());
 
@@ -74,9 +80,9 @@ public class 칵테일 {
 
     public static void dfs(int current){
         visited[current] = true;
-        for(Node node : adj[current]){
+        for(Node node : recipeGraph[current]){
             if(!visited[node.next]){
-                ans[node.next] = (ans[current] * node.q) / node.p;
+                ingredientAmounts[node.next] = (ingredientAmounts[current] * node.q) / node.p;
                 // 재귀
                 dfs(node.next);
             } // if
