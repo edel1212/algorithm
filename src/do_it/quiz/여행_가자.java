31401584c2213a849bb87088250e63dbd8292616
yoo <edel1212@naver.com>
2026-01-11 15:56:08 +0900

    여행가자 재풀이 - 단일 패스

--- src/do_it/quiz/여행_가자.java
+++ src/do_it/quiz/여행_가자.java
@@ -19,7 +19,40 @@ public class 여행_가자 {
         parent = new int[N + 1];
         for (int i = 1; i <= N; i++)  parent[i] = i; // 처음에는 자기 자신이 부모(대표)임
 
+        // 인접 행열을 읽으면서 도시 연결하기 (union)
+        for(int i = 1 ; i <= N ; i++){
+            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+            // 연결 관계 확인
+            for(int j = 1; j <= N; j++){
+                // 0일 땐 이어지지 않음 , 1일 땐 이어짐
+                int isConnected = Integer.parseInt(stringTokenizer.nextToken());
+                if (isConnected == 1) {
+                    // i번 도시와 j번 도시가 연결되어 있다면 한 그룹으로 합침
+                    union(i, j);
+                }
+            } // for
+        } // for
+
+        // 여행 계획 확인하기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 여행 계획에 속한 도시
+        int[] travelPlan = new int[M];
+        // init
+        for(int i = 0 ; i < M ; i++){
+            travelPlan[i] = Integer.parseInt(st.nextToken());
+        } // for
+
+        // 여행 계획의 첫번째를 기준점으로 대표 노드를 가져옴
+        int root = find(travelPlan[0]); // 비교를 하기위해 먼저 가져옴 (기준점)
         boolean isPossible = true;
+
+        for(int i = 1; i < M ; i ++){
+            if(root != find(travelPlan[i])){
+                isPossible = false;
+                break;
+            } // if
+        } // for
+
         bw.write(isPossible ? "YES" : "NO");
         bw.flush();
         bw.close();
@@ -36,7 +69,7 @@ public class 여행_가자 {
     public static int find(int i){
         if(i == parent[i]) return i;
         // 경로 압축
-        return parent[i] = find(i);
+        return parent[i] = find(parent[i]);
     }
 
 }
